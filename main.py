096fc7d21e44d0750f7f52c013c5b67ce264050e
Utku Eren K. <95246846+utkuerenk@users.noreply.github.com>
2024-09-15 21:33:57 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from geminimodule import AISend;import colorama;colorama.init();import json;from os import system
 def main():
     try:
-        system("title Gemterminal")
+        system("title Gemshell")
         while 1:i=input(colorama.Fore.WHITE+"Enter text >> ");print("");returnn=AISend(i);print(colorama.Fore.YELLOW+returnn)
     except Exception as e:input(f"[Error]: {e}")
 if __name__=="__main__":main()
